97cdfe938ed41d94f8cfce7d3c15a4d26d5fd7c5
Krishna Indoria <indoriayt5@gmail.com>
2026-06-04 20:43:13 +0530

    Initial backend setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+require('dotenv').config(); //dotenv fetches all the env variables into process.env
+const main = require('./config/db')
+const cookie =  require('cookie-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(express.json());
+app.use(cookieParser());
+
+main()
+.then( ()=>{
+    app.listen(process.env.PORT,()=>{
+      console.log("Server running at port no:"+process.env.PORT);
+    })
+})
+.catch(err=>console.log("Error occured:"+err));
---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    firstName:{
+        type:String,
+        require:true,
+        minLength:3,
+        maxLength:20
+    },
+    lastName:{
+        type:String,
+        maxLength:20
+    },
+    email:{
+        type:String,
+        unique:true,
+        require:true,
+        trim:true,
+        lowercase:true,
+        immutable:true
+    },
+    age:{
+        type:Number,
+        min:10
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default:'user'
+    },
+    problemsolved:{
+        type:[String]
+    }
+},{
+    timestamps=true
+});
+
+const User = mongoose.model("user",userSchema);
+module.exports = User;
