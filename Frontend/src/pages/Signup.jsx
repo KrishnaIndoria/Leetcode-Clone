5ebe582b573229f7cde3e26ab809ab374908da43
Krishna Indoria <indoriayt5@gmail.com>
2026-06-24 16:49:21 +0530

    react hook form

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,22 +1,39 @@
-import { useEffect, useState } from "react"
+import { useForm } from 'react-hook-form';
 
 function Signup(){
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-
-    const handleSubmit = (e)=>{
-        e.preventDefault(); //this does not refresh the page after form submit(which happens in default,so prevent it.)
-        console.log(name,email,password);
-    }
+    const {register,handleSubmit,formState: { errors },} = useForm();
     return(
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={name} placeholder="FirstName" onChange={(e)=>setName(e.target.value)}></input>
-            <input type="email" value={email} placeholder="Email" onChange={(e)=>setEmail(e.target.value)}></input>
-            <input type="password" value={password} placeholder="password" onChange={(e)=>setPassword(e.target.value)}></input>
-            <button type="submit">Submit</button>
+        <form onSubmit={handleSubmit((data)=>console.log(data))}>
+            <input {...register('firstName')}
+            placeholder='firstname' />
+            <input {...register('email')}
+            placeholder='email' />
+            <input {...register('password')}
+            placeholder='password' />
+            <button type='submit'>Submit</button>
         </form>
     )
 }
 
 export default Signup
+
+// import { useEffect, useState } from "react"
+
+// function Signup(){
+//     const [name,setName] = useState("");
+//     const [email,setEmail] = useState("");
+//     const [password,setPassword] = useState("");
+
+//     const handleSubmit = (e)=>{
+//         e.preventDefault(); //this does not refresh the page after form submit(which happens in default,so prevent it.)
+//         console.log(name,email,password);
+//     }
+//     return(
+//         <form onSubmit={handleSubmit} className="min-h-screen gap-1 flex flex-col justify-center items-center">
+//             <input type="text" value={name} placeholder="FirstName" onChange={(e)=>setName(e.target.value)}></input>
+//             <input type="email" value={email} placeholder="Email" onChange={(e)=>setEmail(e.target.value)}></input>
+//             <input type="password" value={password} placeholder="password" onChange={(e)=>setPassword(e.target.value)}></input>
+//             <button type="submit">Submit</button>
+//         </form>
+//     )
+// }
